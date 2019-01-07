c70c42271f332b75cd5d410881345dee54cb4bdc
Takeshi Kondo <ncukondo@gmail.com>
2019-01-07 17:32:13 +0900

    update package.json

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.common.config.js');
 
-module.exports = {
+module.exports = merge(baseConfig, {
   mode: 'production',
   entry: './src/index.ts',
   devtool: false,
@@ -8,4 +10,4 @@ module.exports = {
     filename: 'bundle.js',
     path: path.join(__dirname, 'product')
   }
-};
+});
